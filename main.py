ccb556676952c4e6ccfebde2c1b52f9a2a15e52a
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-28 15:39:38 +0000

    First commit and test text

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ import numpy as np
 import altair as alt
 import matplotlib as mp
 
+statistics = pd.DataFrame({
+    'Date':[],
+    'Exercise':[],
+    'Weight':[],
+    'Reps':[],
+    'Sets':[]
+})
+
+st.header('OK Gym Stats')
+
+st.write('This is a test page for OK GYM stats.')
