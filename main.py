fdaef5a65f53c6de1ccad98caac8df4c970ff57a
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-28 15:59:49 +0000

    ag_grid import and added to requirements

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 # OK Gym Stats #
 ################
 
+#Import modules
 import streamlit as st
+from st_aggrid import AgGrid as aG
 import pandas as pd
 import numpy as np
 import altair as alt
 import matplotlib as mp
 
+#Data Source
 statistics = pd.DataFrame({
     'Date':[],
     'Exercise':[],
@@ -16,8 +19,12 @@ statistics = pd.DataFrame({
     'Sets':[]
 })
 
+#Page display config
 st.set_page_config(layout='wide')
 
+#Header
 st.header('OK Gym Stats')
 
+#Opening blurb
 st.write('This is a test page for OK GYM stats.')
+
