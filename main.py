e2189a00dc9ff767451a6940958339a5820721e0
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-29 17:51:37 +0000

    update to.csv write link

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if st.button("Add Data"):
         get_data().append({'Date': curdate, "Exercise": exercise, "Weight": weight_kg, "Reps": reps, "Sets": sets})
         stats_df = pd.DataFrame(get_data())
         st.write(stats_df)
-        stats_df.to_csv('https://raw.githubusercontent.com/charlesvarthur/OK_Gym_Stats/main/gym_stats.csv', mode='a', sep=',', date_format='DD-MM-YYYY')
+        stats_df.to_csv('https://1drv.ms/u/s!AvhIY7ZP-KoHd_LLMYlHMN-aTTQ?e=tGupcw', mode='a', sep=',', date_format='DD-MM-YYYY')
     except:
         print(Exception)    
 
