9c4286587e3ed0595ec05ca2b5dd567136e683dc
Charles Arthur <charles.v.arthur@gmail.com>
2023-02-03 10:59:51 +0000

    config

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ import altair as alt
 import matplotlib as mp
 import datetime as dt
 import psycopg2 as pg
+from configparser import ConfigParser, Error
+import config
+
+
+#Function to read the database ini file and verify that the section for postgresql exists. 
+def config(filename=st.secrets, section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
 
 #Run a test connection for PostgreSQL and return the version data. 
 def data_insert():
@@ -16,7 +37,7 @@ def data_insert():
     conn = None
     try:
         # read connection parameters
-        params = st.secrets.postgresql
+        params = config()
 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
@@ -64,3 +85,6 @@ if st.button("Add Data"):
     stats_df = pd.DataFrame(get_data())
     if __name__ == '__main__':
        data_insert()
+
+#Write the csv 
+st.write(stats_df)
