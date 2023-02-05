d7b53429c54c986503e95d29d1c52f1d7950deda
Charles Arthur <charles.v.arthur@gmail.com>
2023-02-05 16:39:38 +0000

    comment out pg_config

--- main.py
+++ main.py
@@ -13,26 +13,26 @@ import config
 
 
 #Function to read the database ini file and verify that the section for postgresql exists. 
-@st.experimental_singleton
-def pg_config():
-    return psycopg2.connect(**st.secrets["postgresql"])
+# @st.experimental_singleton
+# def pg_config():
+#     return psycopg2.connect(**st.secrets["postgresql"])
 
-conn = pg_config()
+# conn = pg_config()
 
-def run_query(query):
-    try:
-        with conn.cursor() as cur:
-            cur.execute(query)
-            return cur.fetchall()
-    except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-    finally:
-            conn.close()
-            print('Database connection closed.')           
+# def run_query(query):
+#     try:
+#         with conn.cursor() as cur:
+#             cur.execute(query)
+#             return cur.fetchall()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#             print(error)
+#     finally:
+#             conn.close()
+#             print('Database connection closed.')           
             
 
-get_pgversion = run_query("Select version()")
-st.write(get_pgversion)
+# get_pgversion = run_query("Select version()")
+# st.write(get_pgversion)
 
 # #Run a test connection for PostgreSQL and return the version data. 
 # def data_insert():
