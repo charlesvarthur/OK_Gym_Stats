a11b61a0a9a0e118f4aeab245b3bfc17481ffd92
Charles Arthur <charles.v.arthur@gmail.com>
2023-02-12 17:08:41 +0000

    test exercise data button write

--- main.py
+++ main.py
@@ -27,59 +27,6 @@ def build():
     finally:
             conn.close()
 
-
-
-
-
-#Function to read the database ini file and verify that the section for postgresql exists local solution. 
-# @st.experimental_singleton
-# def pg_config():
-#     return psycopg2.connect(**st.secrets["postgresql"])
-
-# conn = pg_config()
-
-# def run_query(query):
-#     try:
-#         with conn.cursor() as cur:
-#             cur.execute(query)
-#             return cur.fetchall()
-#     except (Exception, psycopg2.DatabaseError) as error:
-#             print(error)
-#     finally:
-#             conn.close()
-#             print('Database connection closed.')           
-            
-
-# get_pgversion = run_query("Select version()")
-# st.write(get_pgversion)
-
-#Run a test connection for PostgreSQL and return the version data. 
-# def data_insert():
-#     """ Connect to the PostgreSQL database server """
-#     conn = None
-#     try:
-#         # read connection parameters
-#         # connect to the PostgreSQL server
-#         print('Connecting to the PostgreSQL database...')
-#         # create a cursor
-#         cur = conn.cursor()
-# 	# Insert Values
-#         cur.execute("INSERT INTO exercises (col1, col2) VALUES (%s, %s, %s, %s, %s)", (curdate,exercise,weight_kg,reps,sets))
-       
-# 	# close the communication with the PostgreSQL
-#         cur.close()
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print('Database connection closed.')
-
-# #Data Input and DB Write
-# @st.cache(allow_output_mutation=True)
-# def get_data():
-#     return []
-
 #Page display config
 st.set_page_config(layout='wide')
 
@@ -96,14 +43,13 @@ weight_kg = st.slider("Weight in KG", 0, 100)
 reps = st.slider("Reps", 0, 50)
 sets = st.slider("Sets", 0, 30)
 
-st.subheader('Build returns')
-build()
+#st.subheader('Build returns')
+#build()
 
 
+#Cache data for later
 
-# Cache data for later
-# if st.button("Add Data"):
-#     get_data().append({'Date': curdate, "Exercise": exercise, "Weight": weight_kg, "Reps": reps, "Sets": sets})
-#     stats_df = pd.DataFrame(get_data())
-#     if __name__ == '__main__':
-#        data_insert()
+if st.button("Add Data"):
+    exercise_data = []
+    exercise_data.append({'exercise_date': curdate, "exercise": exercise, "weight_kg": weight_kg, "reps": reps, "sets": sets})
+    st.write(exercise_data)
